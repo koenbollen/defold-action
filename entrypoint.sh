e50c8f182e4f16c1f5b5c812de69f3d913f3a80c
Koen Bollen <meneer@koenbollen.nl>
2025-02-19 14:03:59 +0100

    Set big max heap size.

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,7 @@ ls -lph "$GITHUB_WORKSPACE/.defold-cache"
 (
   cd "$GITHUB_WORKSPACE"
 
+  JVM_OPTS=("-Xmx16g")
   EXTRA_OPTS=("--resource-cache-local=$GITHUB_WORKSPACE/.defold-cache/local")
 
   echo "Resolving dependencies..."
@@ -54,17 +55,17 @@ ls -lph "$GITHUB_WORKSPACE/.defold-cache"
   case "$target_platform" in
     "win32" | "windows")
       echo "Building for $target_platform..."
-      java -jar "$bobjar" "${EXTRA_OPTS[@]}" --platform "x86_64-win32" --architectures "x86_64-win32" --archive distclean build bundle  || exit $?
+      java "${JVM_OPTS[@]}" -jar "$bobjar" "${EXTRA_OPTS[@]}" --platform "x86_64-win32" --architectures "x86_64-win32" --archive distclean build bundle  || exit $?
       builddir="$(dirname build/default/*/*.exe)" # this is the only way to find the build dir (it includes the project name which we dont know)
       ;;
     "linux")
       echo "Building for $target_platform..."
-      java -jar "$bobjar" "${EXTRA_OPTS[@]}" --platform "x86_64-linux" --architectures "x86_64-linux" --archive distclean build bundle || exit $?
+      java "${JVM_OPTS[@]}" -jar "$bobjar" "${EXTRA_OPTS[@]}" --platform "x86_64-linux" --architectures "x86_64-linux" --archive distclean build bundle || exit $?
       builddir="$(dirname build/default/*/*.x86_64)" # this is the only way to find the build dir (it includes the project name which we dont know)
       ;;
     "web")
       echo "Building for $target_platform..."
-      java -jar "$bobjar" "${EXTRA_OPTS[@]}" --platform "wasm-web" --architectures "wasm-web" --archive distclean build bundle  || exit $?
+      java "${JVM_OPTS[@]}" -jar "$bobjar" "${EXTRA_OPTS[@]}" --platform "wasm-web" --architectures "wasm-web" --archive distclean build bundle  || exit $?
       builddir="$(dirname build/default/*/index.html)" # this is the only way to find the build dir (it includes the project name which we dont know)
       ;;
     *)
